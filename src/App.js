25cb77cca9e51948f39c79dbed26cbd8571a9377
Md Ibrahim <mdrafi15429@gmail.com>
2022-07-06 23:40:24 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Pages/Home/Header';
 import Navbar from './Pages/Shered/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/Pages/Home/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div  style={{
+            backgroundColor: '#9921E8',
+        }}
+        className="w-screen h-screen">
+            <div>
+                <h2>Hello</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
